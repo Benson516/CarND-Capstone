3f0c2627d6b44bc3058155b3fc0d9f4b15618aa5
Benson516 <benson516@hotmail.com>
2021-02-21 22:41:14 +0800

    Implement the rest of the control()

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -35,7 +35,11 @@ class Controller(object):
         self.wheel_radius = param_dict["wheel_radius"]
         #------------------------------#
 
+        # Variables
+        #------------------------------#
         self.last_time = rospy.get_time()
+        self.last_vel = 0.0
+        #------------------------------#
 
 
     def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
@@ -50,3 +54,29 @@ class Controller(object):
         current_vel = self.vel_lpf.filt(current_vel)
 
         #
+
+        steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+
+        vel_error = linear_vel - current_vel
+        self.last_vel = current_vel
+
+        current_time = rospy.get_time()
+        sample_time = current_time - self.last_time
+        self.last_time = current_time
+
+        throttle = self.throttle_controller.step(vel_error, sample_time)
+        brake = 0.0
+
+
+        if linear_vel == 0.0 and current_vel < 0.1:
+            throttle = 0.0
+            brake = 700 # N-m
+        elif throttle < 0.1 and vel_error < 0.0: # need to decelerate
+            throttle = 0.0
+            ideal_braking_time = 1.0 # sec.
+            decel = max(vel_error*ideal_braking_time, self.decel_limit)
+            brake = abs(decel) * self.vehicle_mass * self.wheel_radius # Torque N-m
+
+        return throttle, brake, steering
+
+
