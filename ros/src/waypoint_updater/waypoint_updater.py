ead0d9ceb3c29ffaa3fda379ada2d2b47384959e
benson516 <bens0516@gmail.com>
2021-03-08 03:06:43 +0000

    Fix a bug about indexing a list

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -170,8 +170,9 @@ class WaypointUpdater(object):
     def distance(self, waypoints, wp1, wp2):
         dist = 0
         dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
+        _length = len(waypoints)
         for i in range(wp1, wp2+1):
-            dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
+            dist += dl(waypoints[wp1%_length].pose.pose.position, waypoints[i%_length].pose.pose.position)
             wp1 = i
         return dist
 
